4c4c925f97b885e6f64e0580ba30260e78d2387d
Seth Frutiger <sethfrutiger@gmail.com>
2022-04-24 19:44:18 -0400

    added project link

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,17 @@ const Projects = () => {
             <div className='section'>
                 <div>
                     <h1 className='section-header'>Projects</h1>
-                    <p className='py-4'>Coming soon. Visit <a className='underline' href='https://github.com/sfrutiger'>Github</a> profile in the meantime.</p>
+                    <ul className='py-4 grid grid-cols-1 sm:grid-cols-2'>
+                        <li>
+                            <h2 className="text-2xl">Easy SOAP</h2>
+                            <p className="my-2">A simple web-based electronic medical records system built on the MERN stack.</p>
+                            <div className="flex flex-col sm:flex-row items-left">
+                            <button className='text-white w-[100px] border-2 px-6 py-3 my-2'><a href='https://easy-soap.herokuapp.com/'>Site</a></button>
+                            <button className='text-white w-[100px] border-2 px-6 py-3 my-2 sm:ml-2'><a href='https://github.com/sfrutiger/easy-soap'>Code</a></button>
+                            </div>
+                        </li>
+                    </ul>
+                    <p className='py-4'>For more projects visit <a className='underline' href='https://github.com/sfrutiger'>Github</a> profile.</p>
                 </div>
             </div>
         </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,7 +6,7 @@ const Skills = () => {
             <div className='section'>
                 <div>
                     <h1 className='section-header'>Skills</h1>
-                    <ul className='py-4 grid grid-cols-2'>
+                    <ul className='py-4 ml-6 mr-0 grid grid-cols-2 list-disc'>
                         <li>JavaScript</li>
                         <li>React</li>
                         <li>React Native</li>
